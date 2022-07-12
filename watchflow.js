1445efa078f6191599b3707d394517cd572b469f
hanchn <jing.yuan@kapeixi.com>
2022-07-12 15:17:15 +0800

    .

--- watchflow.js
+++ watchflow.js
@@ -1,23 +1,39 @@
 var http = require('http')
 var createHandler = require('github-webhook-handler')
-var handler = createHandler({ path: '/test', secret: '123' })
+var handler = createHandler({ path: '/webhook', secret: '' })
+
+function RunCmd(cmd, args, cb) {
+    var spawn = require('child_process').spawn;
+    var child = spawn(cmd, args);
+    var result = '';
+    child.stdout.on('data', function(data) {
+        result += data.toString();
+    });
+    child.stdout.on('end', function() {
+        cb(result)
+    });
+}
 
 http.createServer(function(req, res) {
     handler(req, res, function(err) {
-        res.statusCode = 404
-        res.end('webhooks test !')
+        res.statusCode = 404;
+        res.end('no such location');
     })
 }).listen(7777)
 
 handler.on('error', function(err) {
-    console.error('Error:', err.message)
+    console.error('Error:', err.message);
 })
 
 handler.on('push', function(event) {
     console.log(1111)
     console.log('Received a push event for %s to %s',
         event.payload.repository.name,
-        event.payload.ref)
+        event.payload.ref);
+    var shpath = './start.sh';
+    RunCmd('sh', [shpath], function(result) {
+        console.log(result);
+    })
 })
 
 handler.on('issues', function(event) {
@@ -25,5 +41,5 @@ handler.on('issues', function(event) {
         event.payload.repository.name,
         event.payload.action,
         event.payload.issue.number,
-        event.payload.issue.title)
+        event.payload.issue.title);
 })
