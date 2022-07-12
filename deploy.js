763039e928a3ac09030699b2ef10b93cfe56231e
hanchn <jing.yuan@kapeixi.com>
2022-07-12 15:32:25 +0800

    .

--- deploy.js
+++ deploy.js
@@ -1,6 +1,6 @@
 var http = require('http')
 var createHandler = require('github-webhook-handler')
-var handler = createHandler({ path: '/webhook', secret: '' })
+var handler = createHandler({ path: 'hanchn/test', secret: '' })
 
 function RunCmd(cmd, args, cb) {
     var spawn = require('child_process').spawn;
